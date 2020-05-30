3af50d421f36d3308747263584141e6902f1ee03
Gahan Rakholia <gahan94rakh@gmail.com>
2020-05-30 16:23:18 +0530

    test: add tests for echo router

--- src/echo.rs
+++ src/echo.rs
@@ -1,8 +1,14 @@
-use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 use std::convert::Infallible;
+
+use serde::{Deserialize, Serialize};
 use warp::reply::Json;
-use warp::{Filter, Rejection};
+use warp::{path, post};
+use warp::{Filter, Rejection, Reply};
+
+pub fn routes() -> impl Filter<Extract = impl Reply, Error = warp::Rejection> + Clone {
+    path!("echo").and(post()).and(json_body()).and_then(handler)
+}
 
 #[derive(Serialize, Deserialize)]
 pub struct RequestBody {
@@ -15,4 +21,37 @@ pub fn json_body() -> impl Filter<Extract = (RequestBody,), Error = Rejection> +
 
 pub async fn handler(req: RequestBody) -> Result<Json, Infallible> {
     Ok(warp::reply::json(&req))
+}
+
+#[cfg(test)]
+mod tests {
+    use serde_json::json;
+    use warp::http::StatusCode;
+
+    use super::*;
+
+    #[tokio::test]
+    async fn test_default_handler() {
+        let mut data = HashMap::new();
+        data.insert("foo".to_string(), "bar".to_string());
+        let req = RequestBody { data };
+
+        let filter = routes();
+        let resp = warp::test::request()
+            .method("POST")
+            .path("/echo")
+            .json(&req)
+            .reply(&filter)
+            .await;
+
+        let expected_resp = json!({
+            "data": {
+                "foo": "bar"
+            }
+        })
+        .to_string();
+
+        assert_eq!(resp.status(), StatusCode::OK);
+        assert_eq!(*resp.body(), expected_resp)
+    }
 }
--- src/router.rs
+++ src/router.rs
@@ -7,7 +7,5 @@ use crate::ping;
 pub fn routes() -> impl Filter<Extract = impl Reply, Error = warp::Rejection> + Clone {
     let gets = get().and(path!("ping").and_then(ping::handler));
 
-    let posts = post().and(path!("echo").and(echo::json_body()).and_then(echo::handler));
-
-    gets.or(posts)
+    gets.or(echo::routes())
 }
