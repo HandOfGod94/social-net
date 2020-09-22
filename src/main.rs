b87cce3748b8a8ca58e38b2dc0dd1a4c9278e70f
Gahan Rakholia <gahan94rakh@gmail.com>
2020-09-23 03:33:54 +0530

    feat: add logging back
    additional changes:
    - add editorconfig

--- src/main.rs
+++ src/main.rs
@@ -6,21 +6,23 @@ extern crate log;
 extern crate pretty_env_logger;
 
 use actix_web::{App, HttpServer};
-use diesel::r2d2::{ConnectionManager, Pool};
+use actix_web::middleware::Logger;
 use diesel::PgConnection;
+use diesel::r2d2::{ConnectionManager, Pool};
 
 mod auth;
 mod echo;
 mod ping;
 mod schema;
-// mod user;
 mod router;
 
 type ConnectionPool = Pool<ConnectionManager<PgConnection>>;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-  HttpServer::new(|| App::new().configure(router::route_config))
+  pretty_env_logger::init();
+  info!("Starting Server!!");
+  HttpServer::new(|| App::new().wrap(Logger::default()).configure(router::route_config))
     .bind("127.0.0.1:8080")?
     .run()
     .await
